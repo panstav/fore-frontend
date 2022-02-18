b90789c81ddb7cf46a8b1539188fb0571c3a2c25
Stav Geffen <stavgeffen@gmail.com>
2022-02-18 12:21:26 +0000

    fix(Modal) improves title size and close button position

--- src/components/wrappers/Modal/Modal.js
+++ src/components/wrappers/Modal/Modal.js
@@ -8,13 +8,17 @@ export default function Modal({ title, contextTitle, hideModal, children }) {
 	return <div className="modal is-active">
 		<div className="modal-background" onClick={hideModal}/>
 		<div className="modal-content">
-			<div className="box is-relative">
-				{contextTitle && <div className="has-text-grey-light has-text-weight-bold mb-3 text-wrap">{contextTitle}</div> }
-				<div className="level is-mobile title has-text-grey is-justify-content-space-between">
-					<span>{title}</span>
-					<Close onClick={hideModal}/>
+			<div className="box is-relative p-0">
+				<Close className="is-pulled-right m-1" onClick={hideModal} />
+				<div className="box-inner">
+
+					{contextTitle && <div className="has-text-grey-light has-text-weight-bold mb-3 text-wrap">{contextTitle}</div>}
+
+					<div className="level is-mobile title is-4 has-text-grey">{title}</div>
+
+					{children}
+					
 				</div>
-				{children}
 			</div>
 		</div>
 	</div>;
