8e73ba46a0effa3c50f33e98cd3b4a87651eaf54
Stav Geffen <stavgeffen@gmail.com>
2022-05-11 16:45:42 +0000

    feat(unhideble-modals)

--- src/components/wrappers/Modal/Form.js
+++ src/components/wrappers/Modal/Form.js
@@ -3,7 +3,7 @@ import { useForm, FormProvider } from 'react-hook-form';
 
 import Modal from './Modal';
 
-export default function Form({ onSubmit, autoClose = true, hideModal: unsafeHideModal, render, title, renderContextTitle, ...modalProps }) {
+export default function Form({ onSubmit, hideable, autoClose = true, hideModal: unsafeHideModal, render, title, renderContextTitle, ...modalProps }) {
 
 	const ref = useRef(null);
 
@@ -25,7 +25,7 @@ export default function Form({ onSubmit, autoClose = true, hideModal: unsafeHide
 	}, [ref]);
 
 	return <FormProvider {...form}>
-		<Modal {...{ title, renderContextTitle, hideModal }}>
+		<Modal {...{ title, renderContextTitle, hideModal, hideable }}>
 			<form ref={ref} onSubmit={form.handleSubmit(handleSubmit)}>
 				{render(Object.assign({}, form, modalProps))}
 			</form>
--- src/components/wrappers/Modal/Modal.js
+++ src/components/wrappers/Modal/Modal.js
@@ -1,15 +1,20 @@
+import noop from '../../../../lib/noop';
+
 import { Close } from 'base/Icon';
 
 import useEscape from 'hooks/use-escape.js';
 
-export default function Modal({ title, renderContextTitle, hideModal, children }) {
+export default function Modal({ title, renderContextTitle, hideModal, children, hideable = true }) {
+
+	if (!hideable) hideModal = noop;
+
 	useEscape(hideModal);
 
 	return <div className="modal is-active">
 		<div className="modal-background" onClick={hideModal}/>
 		<div className="modal-content">
 			<div className="box is-relative p-0">
-				<Close className="is-pulled-right m-1" onClick={hideModal} />
+				{ hideable && <Close className="is-pulled-right m-1" onClick={hideModal} /> }
 				<div className="box-inner">
 
 					<div className="has-text-grey-light has-text-weight-bold mb-3 text-wrap">
