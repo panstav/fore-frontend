b2355865d1763a4af138710e8dd34d49787f9a8c
Stav Geffen <stavgeffen@gmail.com>
2022-05-11 16:39:11 +0000

    fix(AddClaim) removes isAnonymous until it explaination is viable

--- src/components/config/Providers/Modal/AddClaim/index.js
+++ src/components/config/Providers/Modal/AddClaim/index.js
@@ -1,17 +1,13 @@
-import Tooltip from 'wrappers/Tooltip';
-
-import Checkbox from 'base/Checkbox';
-
 import AddClaimBody from "./AddClaimBody";
 
-export default function AddClaim({ control }) {
+export default function AddClaim() {
 	return <>
 		<div className="field">
 			<div className="is-relative">
 				<AddClaimBody />
 			</div>
 		</div>
-		<div className="field levem is-justify-content-start">
+		{/* <div className="field levem is-justify-content-start">
 			<Tooltip
 				content={"If you don't want to have your name beside it or if you don't mind letting someone who does."}>
 				<label className="checkbox is-justify-content-flex-start levem">
@@ -19,7 +15,7 @@ export default function AddClaim({ control }) {
 					Anonymous
 				</label>
 			</Tooltip>
-		</div>
+		</div> */}
 		<div className="is-flex is-justify-content-end">
 			<button className="button is-primary">Claim</button>
 		</div>
