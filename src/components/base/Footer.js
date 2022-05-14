d7cd07d7f281df633aeec15a7eb88d7165a02d62
Stav Geffen <stavgeffen@gmail.com>
2022-05-14 09:30:49 +0000

    fix(footer) sets mailto links to use anchors insteasd of pushState Link

--- src/components/base/Footer.js
+++ src/components/base/Footer.js
@@ -2,10 +2,10 @@ import { Link } from 'wouter-preact';
 
 export default function Footer() {
 	return <footer className="has-text-centered is-size-7 pb-3 pt-6">
-		<Link href="mailto:stavgeffen@gmail.com">Report a bug</Link>
-		<MidDot/>
-		<Link href="mailto:stavgeffen@gmail.com">Give feedback</Link>
-		<MidDot/>
+		<a href="mailto:stavgeffen@gmail.com">Report a bug</a>
+		<MidDot />
+		<a href="mailto:stavgeffen@gmail.com">Give feedback</a>
+		<MidDot />
 		<Link href="/privacy-policy">Privacy Policy</Link>
 	</footer>;
 }
