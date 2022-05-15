d0712c26f5aa0ccb4991a36c815c88c43d48748d
Stav Geffen <stavgeffen@gmail.com>
2022-05-15 14:24:43 +0000

    fix(footer) improves responsivity

--- src/components/base/Footer.js
+++ src/components/base/Footer.js
@@ -1,15 +1,24 @@
 import { Link } from 'wouter-preact';
 
+import { Logo } from 'base/Icon';
+
 export default function Footer() {
-	return <footer className="has-text-centered is-size-7 pb-3 pt-6">
-		<a href="mailto:stavgeffen@gmail.com">Report a bug</a>
-		<MidDot />
-		<a href="mailto:stavgeffen@gmail.com">Give feedback</a>
-		<MidDot />
-		<Link href="/privacy-policy">Privacy Policy</Link>
+	return <footer className="pt-6 px-5 pb-3">
+		<div className="is-flex is-align-items-center mb-4">
+			<Logo className="mr-3" />
+			<h2 className="has-text-weight-bold">Fore</h2>
+		</div>
+		<div className="is-size-7">
+			<DotLink dotless={true} href="mailto:stavgeffen@gmail.com">Give feedback</DotLink>
+			<DotLink href="mailto:stavgeffen@gmail.com">Report a bug</DotLink>
+			<DotLink href="/privacy-policy">Privacy Policy</DotLink>
+		</div>
 	</footer>;
 }
 
-function MidDot() {
-	return <span style={{ cursor: 'default', padding: '0 1em', display: 'inline-block' }}>·</span>;
+function DotLink({ dotless, href, children }) {
+	return <span style={{ display: 'inline-block', cursor: 'default' }}>
+		{ !dotless && <span style={{ padding: '0 1em' }}>·</span> }
+		<a href={href}>{children}</a>
+	</span>;
 }
