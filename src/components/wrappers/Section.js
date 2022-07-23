3ea8eefff157f3e03d539b51f9fbc6a793846303
Stav Geffen <stavgeffen@gmail.com>
2022-07-23 12:47:33 +0000

    fix(Section) supports arbitrary props

--- src/components/wrappers/Section.js
+++ src/components/wrappers/Section.js
@@ -1,6 +1,6 @@
 import classnames from 'classnames';
 
-export default function Section({ className = '', withTopMargin, noSidePadding, children }) {
+export default function Section({ className = '', withTopMargin, noSidePadding, children, ...props }) {
 	const containerClasses = classnames('container', className, noSidePadding ? '' : 'px-3', withTopMargin ? 'mt-6' : '');
-	return <div className={containerClasses}>{children}</div>;
+	return <div {...props} className={containerClasses}>{children}</div>;
 }
