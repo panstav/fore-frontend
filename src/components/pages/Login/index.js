de0d0211af5b5a9d04fe466ab21d126b21ca25df
Stav Geffen <stavgeffen@gmail.com>
2022-07-23 12:23:23 +0000

    fix(Login) redirects logged-in users away from login

--- src/components/pages/Login/index.js
+++ src/components/pages/Login/index.js
@@ -1,12 +1,28 @@
+import { connect } from 'unistore/preact';
+import { Redirect } from 'wouter-preact';
+
+import isAuth from 'lib/is-auth';
+
 import Component from './Login';
 
-import { urls } from 'constants';
+import { urls, roles } from 'constants.js';
 
-export default function Login() {
+export default connect(mapStateToProps)(Login);
+
+function Login({ isLoggedIn }) {
+
+	// redirect a user to the homepage if they are already logged in
+	if (isLoggedIn) return <Redirect to={'/'} replace={true} />;
 
 	const props = {
 		authViaGoogleUrl: `${urls.api}/auth/google`
 	};
 
 	return Component(props);
+}
+
+function mapStateToProps({ user }) {
+	return {
+		isLoggedIn: isAuth(user.role, { minimum: roles.order[1] })
+	};
 }
