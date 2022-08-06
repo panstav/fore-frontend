0406e0f7d2ba57394fca11e6d1ba613acf07fbec
Stav Geffen <stavgeffen@gmail.com>
2022-08-06 11:11:43 +0000

    fix(homepage) adds some letter-spacing to main title

--- src/components/pages/Home/PromotionalHomepage.js
+++ src/components/pages/Home/PromotionalHomepage.js
@@ -35,7 +35,7 @@ export default function PromotionalHomepage () {
 	return <>
 		<Section className="has-text-centered">
 			<Logo className="mt-5" style={{ width: '10rem', height: '10rem' }} />
-			<h1 className="title is-1 mt-6">Fore</h1>
+			<h1 className="title is-1 mt-6" style={{ letterSpacing: '0.05em' }}>Fore</h1>
 			<p className="has-text-weight-bold has-text-primary">Where collaborative truth-seeking happens.</p>
 			<Access minimum={roles.order[1]}
 				onFail={() => <SignupCTA whatFor="to get started." className="mt-6" />}
