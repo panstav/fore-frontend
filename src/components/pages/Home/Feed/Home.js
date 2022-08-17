9320ba5e2b1c0cee027f84eb014c7607abe9d373
Stav Geffen <stavgeffen@gmail.com>
2022-08-17 15:27:38 +0000

    fix(Home) lets Claim content overflow without elipsis

--- src/components/pages/Home/Feed/Home.js
+++ src/components/pages/Home/Feed/Home.js
@@ -20,10 +20,8 @@ export default function Home({ claims, createNewClaim, isLoading }) {
 				{claims.map(({ id, content, createdAtTimeAgo }) => {
 					return <Link href={`/claim/${id}`} key={id}>
 						<div className="box is-clickable">
-							<div className="levem">
-								<div className="is-size-7 has-text-grey-light" style="width: 5.5ch; min-width: 5.5ch; line-height: 1;">{createdAtTimeAgo}</div>
-								<div className="mr-2 text-wrap is-size-6 is-flex-grow-1 has-text-weight-bold" style="line-height: 1;">{content}</div>
-							</div>
+							<div className="is-size-7 has-text-grey-light mb-1" style="width: 5.5ch; min-width: 5.5ch; line-height: 1;">{createdAtTimeAgo}</div>
+							<div className="mr-2 is-size-6 is-flex-grow-1 has-text-weight-bold">{content}</div>
 						</div>
 					</Link>;
 				})}
