cda3af95fd2165cb179c6c7bf5ea101713309eb9
Stav Geffen <stavgeffen@gmail.com>
2022-08-27 13:11:26 +0000

    fix(ClaimDetail) aligns avatar with claim content

--- src/components/pages/ClaimDetail/ClaimDetail.js
+++ src/components/pages/ClaimDetail/ClaimDetail.js
@@ -14,7 +14,7 @@ export default function ClaimDetail({ content, author, createdAtTimeAgo }) {
 			<h1 className="title mt-2 mb-3">{content}</h1>
 			<div className="is-flex is-align-items-center is-size-7">
 				<div className="fore-claim-owner levem is-justify-content-start mb-0 mr-3">
-					<img className="mx-2 is-round" src={author.profileImageUrl} alt={author.name} />
+					<img className="mr-2 is-round" src={author.profileImageUrl} alt={author.name} />
 					<span>{author.name}</span>
 				</div>
 				<div className="has-text-grey-light">{createdAtTimeAgo}</div>
