1385e044a9606c75597b4098c04de4d006deade6
Stav Geffen <stavgeffen@gmail.com>
2022-08-28 16:32:54 +0000

    fix(localstorage) adds error on parameters given to get/set

--- src/services/localstorage.js
+++ src/services/localstorage.js
@@ -1,15 +1,21 @@
+// a master key is used to store all fore data in localstorage
 const masterKey = 'foreis';
 
-export default {
+export default { get, set };
 
-	set(key, value) {
-		const previousValue = JSON.parse(localStorage.getItem(masterKey)) || {};
-		const newValue = Object.assign(previousValue, { [key]: value });
-		localStorage.setItem(masterKey, JSON.stringify(newValue));
-	},
+function get(key, fallback = {}) {
+	if (!key) throw new Error('key is required');
+	if (key.includes('.')) throw new Error('dot notation is not supported');
+	
+	return JSON.parse(localStorage.getItem(masterKey))[key] || fallback;
+}
 
-	get(key, fallback = {}) {
-		return JSON.parse(localStorage.getItem(masterKey))[key] || fallback;
-	}
+function set(key, value) {
+	if (!key) throw new Error('key is required');
+	if (!value) throw new Error('value is required');
+	if (key.includes('.')) throw new Error('dot notation is not supported');
 
-};
+	const previousValue = get(key);
+	const newValue = Object.assign(previousValue, { [key]: value });
+	localStorage.setItem(masterKey, JSON.stringify(newValue));
+}
