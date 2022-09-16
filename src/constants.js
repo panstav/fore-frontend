8cc307266c2220e0fe795120fbd3818bb118b116
Stav Geffen <stavgeffen@gmail.com>
2022-09-16 12:37:59 +0000

    fix(constants) simplifies self origin

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,9 @@
 export const urls = {
-	frontEnd: 'https://fore.is',
+	frontEnd: window.origin,
 	api: 'https://api.fore.is'
 };
 
 if (process.env.NODE_ENV !== 'production') {
-	if (process.env.CODESPACE_NAME) urls.frontEnd = `https://${process.env.CODESPACE_NAME}.githubpreview.dev`;
-	if (process.env.GITPOD_WORKSPACE_URL) urls.frontEnd = `${process.env.GITPOD_WORKSPACE_URL.replace('https://', 'https://1234-')}`;
 	urls.api = 'https://fore-backend-dev-l6jtvpnuaa-uc.a.run.app';
 	// urls.api = 'https://panstav-fore-backend-qwvqv7q2pxx-3000.githubpreview.dev';
 	// urls.api = 'https://3000-panstav-forebackend-zm4h0qr6aac.ws-eu43.gitpod.io';
