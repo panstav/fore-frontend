2ba63fb544d6fef90510bd107d352af52282ff26
Stav Geffen <stavgeffen@gmail.com>
2022-09-16 20:37:37 +0000

    style(ClaimDetail.js)

--- src/components/pages/ClaimDetail/ClaimDetail.js
+++ src/components/pages/ClaimDetail/ClaimDetail.js
@@ -3,7 +3,7 @@ import Section from 'wrappers/Section';
 import ClaimUsedAt from './ClaimUsedAt';
 import ClaimsUsedHere from './ClaimsUsedHere';
 
-export default function ClaimDetail({ content, author, createdAtTimeAgo }) {
+export default function ClaimDetail(props) {
 	return <>
 
 		<Section className="mt-3">
@@ -11,17 +11,23 @@ export default function ClaimDetail({ content, author, createdAtTimeAgo }) {
 		</Section>
 
 		<Section withTopMargin={true}>
-			<h1 className="title mt-2 mb-3">{content}</h1>
-			<div className="is-flex is-align-items-center is-size-7">
-				<div className="fore-claim-owner levem is-justify-content-start mb-0 mr-3">
-					<img className="mr-2 is-round" src={author.profileImageUrl} alt={author.name} />
-					<span>{author.name}</span>
-				</div>
-				<div className="has-text-grey-light">{createdAtTimeAgo}</div>
-			</div>
+			<ClaimContent {...props} />
 		</Section>
 
 		<ClaimsUsedHere/>
 
+	</>;
+}
+
+function ClaimContent({ content, author, createdAtTimeAgo }) {
+	return <>
+		<h1 className="title mt-2 mb-3">{content}</h1>
+		<div className="is-flex is-align-items-center is-size-7">
+			<div className="fore-claim-owner levem is-justify-content-start mb-0 mr-3">
+				<img className="mr-2 is-round" src={author.profileImageUrl} alt={author.name} />
+				<span>{author.name}</span>
+			</div>
+			<div className="has-text-grey-light">{createdAtTimeAgo}</div>
+		</div>
 	</>;
 }
