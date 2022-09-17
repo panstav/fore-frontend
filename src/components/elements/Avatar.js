97cd80bec25527d4eaa6a8b478f2516cae4ba96f
Stav Geffen <stavgeffen@gmail.com>
2022-09-17 16:16:17 +0000

    fix(Avatar) defaults to thispersondoesnotexist

--- src/components/elements/Avatar.js
+++ src/components/elements/Avatar.js
@@ -1,3 +1,3 @@
 export default function Avatar({ userId, alt, style }) {
-	return <img className="mr-2 is-round" src={`https://storage.googleapis.com/fore-www/avatars/${userId}.jpg`} {...{ alt }} {...{style}} />;
+	return <img className="mr-2 is-round" src={userId ? `https://storage.googleapis.com/fore-www/avatars/${userId}.jpg` : 'https://thispersondoesnotexist.com/image'} {...{ alt }} {...{style}} />;
 }
