dac16ec90ed7729a3d9dda53ca9725c10be552b0
Stav Geffen <stavgeffen@gmail.com>
2022-09-23 23:00:14 +0000

    fix(useBooleanState) fixes toggle callback

--- src/hooks/use-boolean-state.js
+++ src/hooks/use-boolean-state.js
@@ -2,6 +2,6 @@ import { useCallback, useState } from 'preact/compat';
 
 export default function useBooleanState(initialState) {
 	const [state, setState] = useState(!!initialState);
-	const toggle = useCallback((bool = state) => setState(!!bool), [state]);
+	const toggle = useCallback((bool = !state) => setState(!!bool), [state]);
 	return [state, () => toggle(true), () => toggle(false), toggle];
 }
