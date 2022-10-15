6241296639fdfc621303fd0324d426ed387f5b11
Stav Geffen <stavgeffen@gmail.com>
2022-10-15 08:21:37 +0000

    fix(Connect) improves header

--- src/components/pages/Connect/Connect.js
+++ src/components/pages/Connect/Connect.js
@@ -10,7 +10,7 @@ export default function Connect({ connectingMethods, connectionMethod }) {
 	return <>
 		<Section>
 			<div className="box is-small m-auto panel is-primary">
-				<h1 className="title is-size-3 has-text-centered mb-5">Connect to Fore</h1>
+				<h1 className="title is-size-3 has-text-centered my-3">Fore</h1>
 
 				<div className="has-text-centered panel-tabs">
 					{connectingMethods.map(({ name, isActive, switchTo }) => {
