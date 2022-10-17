64c07fe19a4fb86f6f53cabf677e80b6e1aa0f02
Stav Geffen <stavgeffen@gmail.com>
2022-10-17 11:41:35 +0000

    fix(identify) sets data instead of merging it

--- src/components/config/Providers/Auth/actions.js
+++ src/components/config/Providers/Auth/actions.js
@@ -2,10 +2,9 @@ import api from 'services/xhr';
 
 export default {
 
-	async identify({ user }) {
-		const { user: loadedUser, spaces } = await api.identify();
-		const updatedUser = Object.assign({}, user, loadedUser);
-		return { user: updatedUser, spaces };
+	async identify() {
+		const { user = {}, spaces = [] } = await api.identify();
+		return { user, spaces };
 	}
 
 };
