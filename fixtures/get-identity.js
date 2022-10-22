25c8679b669b9fde4e099a685f8a25b502b85301
Stav Geffen <stavgeffen@gmail.com>
2022-10-22 11:41:42 +0000

    fix(fixtures) updates identify mock

--- fixtures/get-identity.js
+++ fixtures/get-identity.js
@@ -2,9 +2,11 @@ import { roles } from 'constants';
 
 export default function getIdentity () {
 	return {
-		id: '123',
-		// role: roles.GUEST
-		// role: roles.MEMBER
-		role: roles.ADMIN
+		user: {
+			id: '123',
+			// role: roles.GUEST
+			// role: roles.MEMBER
+			role: roles.ADMIN
+		}
 	};
 }
