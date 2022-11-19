9a34d8728d662c2d0d455880eca15a96512d4497
Stav Geffen <stavgeffen@gmail.com>
2022-11-19 17:59:15 +0000

    fix(CurrentFeatures) updates copy

--- src/components/compounds/CurrentFeatures.js
+++ src/components/compounds/CurrentFeatures.js
@@ -4,14 +4,10 @@ export default function CurrentFeatures() {
 			<h2>At the moment</h2>
 			<h3 className="is-size-6">Registered users can:</h3>
 			<ul>
-				<li>Support the Closed Beta by submitting Claims</li>
-				<li>Signup for updates</li>
-			</ul>
-			<h3 className="is-size-6">Closed Beta users can also:</h3>
-			<ul>
-				<li>Browse existing Claims</li>
+				<li>Browse recent Claims</li>
 				<li>Support or Oppose to existing Claims</li>
-				<li>Vote for the best support / opposition to a Claim</li>
+				<li>Give power to the best support / opposition to any Claim</li>
+				<li>Rephrase existing Claims</li>
 			</ul>
 		</div>
 	</div>;
