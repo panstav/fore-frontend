85a7cbb180e7fe6f563cb753baef8c761a07d054
Stav Geffen <stavgeffen@gmail.com>
2022-11-20 18:27:32 +0000

    fix(SpaceSelector) sets same-space clicking to navigate to space main page when not on it

--- src/components/config/TopNav/SpaceSelector/index.js
+++ src/components/config/TopNav/SpaceSelector/index.js
@@ -1,4 +1,5 @@
 import { useCallback } from 'preact/compat';
+import { useLocation } from 'wouter-preact';
 import { connect } from 'unistore/preact';
 
 import isAuth from 'lib/is-auth';
@@ -19,6 +20,15 @@ function SpaceSelector({ currentSpace, setCurrentSpace, availableSpaces, userRol
 		closeDropdown();
 	}, [closeDropdown]);
 
+	const [location, setLocation] = useLocation();
+
+	const handleSetCurrentSpace = useCallback((nextSpaceId) => {
+		if (currentSpace !== nextSpaceId) return;
+		const nextSpaceUrl = `/space/${currentSpace}`;
+		if (location !== nextSpaceUrl) return setLocation(nextSpaceUrl);
+		setCurrentSpace(nextSpaceId);
+	}, [currentSpace, setLocation]);
+
 	// avoid rendering dropdown when no Space was yet to be determined
 	if (!currentSpace){
 		setCurrentSpace();
@@ -26,7 +36,7 @@ function SpaceSelector({ currentSpace, setCurrentSpace, availableSpaces, userRol
 	}
 
 	const props = {
-		setCurrentSpace,
+		setCurrentSpace: handleSetCurrentSpace,
 		currentSpaceName: currentSpace.name,
 		availableSpaces: availableSpaces.map(attachHrefDisabled),
 		isOpenDropdown,
@@ -41,7 +51,7 @@ function SpaceSelector({ currentSpace, setCurrentSpace, availableSpaces, userRol
 		// item should appear unclickable to unauthenticated users
 		space.disabled = !isAuth(userRole, { minimum: space.minRole });
 		// disabled items + the items that direct to the current page - don't need a link
-		space.href = (space.disabled || (currentSpace && space.id === currentSpace.id))
+		space.href = space.disabled
 			? null
 			// link to the space, unless it's the public space, then link to the home page
 			: space.id === 'public' ? '/' : `/space/${space.id}`;
