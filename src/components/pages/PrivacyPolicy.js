c35f6d051f0bee15a4b5e8a00694974536b72234
Stav Geffen <stavgeffen@gmail.com>
2022-11-24 17:47:26 +0000

    fix(PrivacyPolicy) adds email, some fixes

--- src/components/pages/PrivacyPolicy.js
+++ src/components/pages/PrivacyPolicy.js
@@ -1,24 +1,26 @@
 import Section from 'wrappers/Section';
 
 export default function PrivacyPolicy() {
-	return <Section>
+	return <Section className='is-medium'>
 
 		<div className="box m-auto mt-6">
 			<div className="content">
 
 				<h1>Privacy Policy</h1>
 
-				<p>At Fore, accessible from https://fore.is, we take the privacy of our visitors seriously. This Privacy Policy document contains straightforward outline of the information that is recorded by Fore.</p>
+				<p>At Fore, accessible from https://fore.is, we take the privacy of our visitors seriously. This Privacy Policy document contains straight-forward outline of the information that is recorded by Fore.</p>
 
 				<h2>Consent</h2>
 
-				<p>By using our website / web application, you hereby consent to our Privacy Policy and agree to its terms.</p>
+				<p>By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p>
 
 				<h2>Information we collect</h2>
 
-				<p>When you navigate and interact with the website and use the web application, we will collect anonymized usage information.</p>
+				<p>When you navigate and interact with and use the website, we will collect anonymized usage information.</p>
 
 				<p>When you register for an account with Google, we will ask for your contact information, including items such as name, email address, and the profile image that is associated with your Google account.</p>
+
+				<p>We do not mix the two. Identifiable information is stored separately from the anonymized usage information and nothing is stored that can be used to match the two.</p>
 
 				<h2>How we use your information</h2>
 
@@ -42,6 +44,10 @@ export default function PrivacyPolicy() {
 				<p>The right to erasure – You have the right to request that we erase your personal data, under certain conditions.</p>
 
 				<p>If you would like to exercise any of these rights, or if you have additional questions or require more information about our Privacy Policy - do not hesitate to contact us.</p>
+
+				<h2>Contact information</h2>
+
+				<p>For any questions or concerns regarding the privacy policy, please send us an email to <a href="mailto:hello@fore.is">hello@fore.is</a>.</p>
 
 			</div>
 		</div>
