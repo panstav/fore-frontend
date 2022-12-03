91c618d7f7f802083e2c4652d8ce2956c9e49f4d
Stav Geffen <stavgeffen@gmail.com>
2022-12-03 12:06:34 +0000

    fix(AddClaim) extracts EscapedContent

--- src/components/config/Providers/Modal/AddClaim/index.js
+++ src/components/config/Providers/Modal/AddClaim/index.js
@@ -6,6 +6,7 @@ import Tooltip from 'wrappers/Tooltip';
 import Checkbox from 'elements/Checkbox';
 
 import AddClaimBody from "./AddClaimBody";
+import EscapedContent from './EscapedContent';
 import Guidelines from './Guidelines';
 import CurrentSpace from './CurrentSpace';
 
@@ -34,29 +35,4 @@ export default function AddClaim({ control, copiedContent }) {
 			</button>
 		</div>
 	</>;
-}
-
-function EscapedContent() {
-	const { control, getValues } = useFormContext();
-	useWatch({ control, name: 'content' });
-
-	const content = getValues('content') || '';
-	if (!content) return null;
-
-	// check copiedContent works
-	const escapedCharacters = Array.from(content.matchAll(sanitizedRegExp))
-		.map((match) => match[0])
-		.reduce((accu, character) => {
-			if (!accu.includes(character)) accu.push(character);
-			return accu;
-		}, []);
-
-	if (!escapedCharacters.length) return null;
-
-	return <p className="notification is-warning is-small">
-		<span className='mr-2'>The following characters are not supported and will not be saved:</span>
-		{escapedCharacters.map((character) => {
-			return <span key={character} className="tag is-warning is-light has-text-weight-bold ml-1" style={{ width: '1.5ch' }}>{character}</span>;
-		})}
-	</p>;
 }
