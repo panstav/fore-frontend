04ad5eb141150d7265a1c65c02ea4f4944652075
Stav Geffen <stavgeffen@gmail.com>
2023-01-09 16:10:01 +0200

    fix(setCurrentSpace) moves Space setting to onRouteChange
    instead of repeating on each relevant page

--- src/components/config/Providers/Auth/actions.js
+++ src/components/config/Providers/Auth/actions.js
@@ -4,12 +4,6 @@ export default {
 
 	async identify() {
 		const { user = {}, spaces = [], notifications = [] } = await api.identify();
-
-		// mark public as the current space if no other space is current
-		if (!spaces.find((space) => space.isCurrent)) {
-			spaces.find((space) => space.id === 'public').isCurrent = true;
-		}
-
 		return { user, spaces, notifications };
 	}
 
--- src/components/pages/SpaceDetail/index.js
+++ src/components/pages/SpaceDetail/index.js
@@ -11,14 +11,11 @@ import Component from './SpaceDetail.js';
 
 export default connect(mapStateToProps, actions)(Space);
 
-function Space({ getSpaceDetail, spaceId, isDetailed, isSetAsCurrent, setCurrentSpace, name }) {
+function Space({ getSpaceDetail, spaceId, isDetailed, name }) {
 
 	// if we're here for the public feed and we're not at home - redirect to home
 	const [location] = useLocation();
 	if (location !== '/' && spaceId === 'public') return <Redirect to="/" replace={true} />;
-
-	// inform the app that we're in this space even if we already have the data
-	if (isDetailed && !isSetAsCurrent) setCurrentSpace(spaceId);
 
 	useEffectUntil(() => getSpaceDetail(spaceId), [isDetailed]);
 
