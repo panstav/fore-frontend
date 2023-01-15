be784eee088a91d7dfb83a1b85334bc1e888e9e3
Stav Geffen <stavgeffen@gmail.com>
2023-01-15 14:59:10 +0200

    fix(SpaceSelector) resets dropdown icon

--- src/components/elements/Icon/index.js
+++ src/components/elements/Icon/index.js
@@ -38,7 +38,7 @@ export const Copy = (props) => <Icon size={460} {...props}>
 	<path d="M288.339,139.998H34.068c-18.122,0-32.865,14.743-32.865,32.865v254.272C1.204,445.257,15.946,460,34.068,460h254.272     c18.122,0,32.865-14.743,32.865-32.864V172.863C321.206,154.741,306.461,139.998,288.339,139.998z M288.341,430H34.068     c-1.58,0-2.865-1.285-2.865-2.864V172.863c0-1.58,1.285-2.865,2.865-2.865h254.272c1.58,0,2.865,1.285,2.865,2.865v254.273h0.001     C291.206,428.715,289.92,430,288.341,430z" />
 </Icon>;
 
-export const DownArrow = (props) => <ArrowFull rotate={180} {...props} />;
+export const DownArrow = (props) => <Arrow rotate={0} {...props} />;
 
 export const Dropdown = ({ isVertical, ...props }) => {
 	return <Icon size={290} svgProps={{ style: { rotate: `${isVertical?'90':'0'}deg` } }} {...props}>
