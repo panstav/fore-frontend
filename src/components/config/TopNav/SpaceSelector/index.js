138dc123bbed09372e3bf6a34e613b7b583527e0
Stav Geffen <stavgeffen@gmail.com>
2023-01-20 15:04:56 +0200

    fix(TopNav) adds CreateSpace to MobileMenu too

--- src/components/config/TopNav/SpaceSelector/index.js
+++ src/components/config/TopNav/SpaceSelector/index.js
@@ -2,35 +2,16 @@ import { useCallback, useEffect, useRef } from 'preact/compat';
 import { useLocation } from 'wouter-preact';
 import { connect } from 'unistore/preact';
 
-import useModal from 'hooks/use-modal';
 import useBooleanState from 'hooks/use-boolean-state';
-
-import { SharedSpace, PrivateSpace } from "elements/Icon";
 
 import actions from './actions';
 
+import { spacesSorter } from '../index';
 import Component from './SpaceSelector';
-
-import { roles } from 'constants';
-
-const spaceTypes = [
-	{
-		name: "shared",
-		label: "Shared Space",
-		description: "Empower group thinking",
-		icon: SharedSpace
-	},
-	{
-		name: "private",
-		label: "Private Space",
-		description: "Think for yourself",
-		icon: PrivateSpace
-	}
-];
 
 export default connect(mapStateToProps, actions)(SpaceSelector);
 
-function SpaceSelector({ isAdmin, currentSpace, setCurrentSpace, spaces, createSpace }) {
+function SpaceSelector({ currentSpace, setCurrentSpace, spaces, createSpace }) {
 
 	const [isOpenDropdown, , closeDropdown, toggleDropdown] = useBooleanState(false);
 	const dropdownRef = useDropdownFocus(closeDropdown);
@@ -43,16 +24,6 @@ function SpaceSelector({ isAdmin, currentSpace, setCurrentSpace, spaces, createS
 		setCurrentSpace(nextSpaceId);
 	}, [currentSpace, setLocation]);
 
-	const hasCreatedBothTypes = !isAdmin && spaces.filter((space) => space.userRole === roles.ADMIN).map(({ type }) => type).length === spaceTypes.length;
-	const [newSpaceModalProps, openNewSpaceModal] = useModal({
-		hasCreatedBothTypes,
-		spaceTypes: spaceTypes.map(attachExisting),
-		onSubmit: async (data) => {
-			const spaceId = await createSpace(data);
-			setLocation(`/space/${spaceId}`);
-		}
-	});
-
 	// avoid rendering dropdown when no Space was yet to be determined
 	if (!currentSpace){
 		setCurrentSpace();
@@ -62,24 +33,15 @@ function SpaceSelector({ isAdmin, currentSpace, setCurrentSpace, spaces, createS
 	const props = {
 		setCurrentSpace: handleSetCurrentSpace,
 		currentSpaceName: currentSpace.name,
-		availableSpaces: spaces.map(attachHref).sort(spacesSort),
+		availableSpaces: spaces.map(attachHref).sort(spacesSorter),
 		isOpenDropdown,
 		// ignore event data triggering the toggle
 		toggleDropdown: () => toggleDropdown(),
 		dropdownRef,
-		newSpaceModalProps,
-		createSpace: () => openNewSpaceModal()
+		createSpace
 	};
 
 	return Component(props);
-
-	function attachExisting(type) {
-		// let the admin create both types even if they already have a space of that type
-		if (isAdmin) return type;
-		// attach the existing space of this type, if any
-		type.existingSpace = spaces.find((existingSpace) => existingSpace.type === type.name);
-		return type;
-	}
 
 }
 
@@ -89,11 +51,10 @@ function attachHref(space) {
 	return space;
 }
 
-function mapStateToProps({ spaces, claims, user }) {
+function mapStateToProps({ spaces, claims }) {
 	const currentSpace = spaces.find((space) => space.isCurrent);
 	const currentSpaceObj = currentSpace ? { id: currentSpace.id, name: currentSpace.name } : null;
 	return {
-		isAdmin: user.role === roles.ADMIN,
 		currentSpace: currentSpaceObj,
 		spaces,
 		// just to trigger a re-render when the user first navigates to a claim page
@@ -116,12 +77,4 @@ function useDropdownFocus(cb) {
 		if (ref.current && !ref.current.contains(event.target)) cb();
 	}
 
-}
-
-function spacesSort (a, b) {
-	// 'public' is always first
-	if (a.id === 'public') return -1;
-	if (b.id === 'public') return 1;
-	// then sort by name
-	return a.name.localeCompare(b.name);
 }
--- src/components/config/TopNav/actions.js
+++ src/components/config/TopNav/actions.js
@@ -1,8 +1,30 @@
+import api from 'services/xhr';
+import trackEvents from 'services/track-events';
+
+import notify from 'lib/notify';
+
+import { notifications } from 'constants';
+
 export default {
 
 	toggleMainMenu: ({ menus }) => {
 		menus.main = !menus.main;
 		return { menus };
+	},
+
+	async createSpace({ spaces }, space) {
+		space.type = space.type || 'shared';
+
+		const notificationId = notify(notifications.NEW_SPACE_SENT);
+
+		const fullSpace = await api.addSpace(space);
+
+		trackEvents('create_space', { spaceId: fullSpace.id });
+
+		notify(notifications.NEW_SPACE_CREATED, { _id: notificationId, spaceId: fullSpace.id });
+
+		spaces.push(fullSpace);
+		return { spaces };
 	}
 
 }
