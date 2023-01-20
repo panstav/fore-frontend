9076f0a8945e1c701124bb78fe2bbf8c1230d0bf
Stav Geffen <stavgeffen@gmail.com>
2023-01-20 15:30:55 +0200

    fix(Sentry) enables Replay

--- src/services/js-tracking.js
+++ src/services/js-tracking.js
@@ -4,11 +4,18 @@ import { BrowserTracing } from "@sentry/tracing";
 if (process.env.NODE_ENV === 'production') {
 	Sentry.init({
 		dsn: "https://71380ef9d52d4213b6ba11854e8f0d1a@o44797.ingest.sentry.io/4504102586875904",
-		integrations: [new BrowserTracing()],
 
-		// Set tracesSampleRate to 1.0 to capture 100%
-		// of transactions for performance monitoring.
-		// We recommend adjusting this value in production
+		integrations: [
+			new BrowserTracing(),
+			new Sentry.Replay({
+				// Additional SDK configuration goes in here, for example:
+				maskAllText: true,
+				blockAllMedia: true,
+			})
+		],
+
 		tracesSampleRate: 0.1,
+		replaysSessionSampleRate: 0.1,
+		replaysOnErrorSampleRate: 1.0
 	});
 }
