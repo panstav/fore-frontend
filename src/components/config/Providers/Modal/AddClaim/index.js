ea3f9e1cb52c2c6c0c19bc3584c5dedb99ffe717
Stav Geffen <stavgeffen@gmail.com>
2023-01-26 22:05:00 +0200

    fix(PrivateSpace) removes anonymous claim checkbox and guidelines

--- src/components/config/Providers/Modal/AddClaim/index.js
+++ src/components/config/Providers/Modal/AddClaim/index.js
@@ -1,3 +1,5 @@
+import { connect } from "unistore/preact"
+
 import TrimmedInput from "elements/TrimmedInput";
 
 import Anonymous from './Anonymous';
@@ -5,14 +7,22 @@ import EscapedContent from './EscapedContent';
 import Guidelines from './Guidelines';
 import CurrentSpace from './CurrentSpace';
 
-export default function AddClaim({ copiedContent }) {
+const Connector = connect(mapStateToProps)(AddClaim);
+
+// export a simple function instead of a connect() invocation
+export default ({ copiedContent }) => {
+	return <Connector copiedContent={copiedContent} />;
+};
+
+function AddClaim({ isntPrivateSpace, copiedContent }) {
+	const textAreaClasses = isntPrivateSpace ? 'mb-2' : null;
 	return <>
 		<div className="fore-new-claim-body field p-2">
-			<TrimmedInput name="content" type="textarea" maxLength={240} defaultValue={copiedContent} className="mb-2" inputClasses="pb-5" />
-			<Anonymous className="ml-1" />
+			<TrimmedInput name="content" type="textarea" maxLength={240} defaultValue={copiedContent} className={textAreaClasses} inputClasses="pb-5" />
+			{isntPrivateSpace && <Anonymous className="ml-1" />}
 		</div>
 		<EscapedContent />
-		<Guidelines />
+		{isntPrivateSpace && <Guidelines />}
 		<div className="is-flex is-justify-content-end">
 			<button className="button is-primary">
 				<span>Claim on&nbsp;</span>
@@ -20,4 +30,11 @@ export default function AddClaim({ copiedContent }) {
 			</button>
 		</div>
 	</>;
+}
+
+function mapStateToProps({ spaces }) {
+	const isntPrivateSpace = spaces.find(space => space.isCurrent)?.type !== 'private';
+	return {
+		isntPrivateSpace
+	};
 }
