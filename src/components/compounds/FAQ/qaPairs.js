340176cdda07956cb1f28b8b0c3185c4e926a055
Stav Geffen <stavgeffen@gmail.com>
2023-02-06 15:25:55 +0200

    fix(FAQ) improves copy

--- src/components/compounds/FAQ/qaPairs.js
+++ src/components/compounds/FAQ/qaPairs.js
@@ -1,15 +1,15 @@
 const qaPairs = [
 	{
 		question: 'What is a Claim?',
-		answer: 'Claims are the basic units of the Fore, much like Posts are the basic units of most social networks. Instead of replying, you can support and/or oppose any Claim by creating a new one, or by using one of your existing Claims.'
+		answer: 'Claims are the basic units of the Fore, much like Posts are the basic units of many social networks. On a Claim\'s page you can reply to a Claim by creating a support or an opposition Claim, or by using one of your existing Claims.'
 	},
 	{
 		question: 'How can Fore empower group thinking?',
-		answer: 'The Public Space on Fore is a place for open discussion, debate and discovery. By creating a Space for your group you will be able to focus and develop on your common needs and goals and avoid public opinion on your common assumptions and axioms.'
+		answer: 'The Public Space on Fore is a place for open discussion, debate and discovery. Create a Shared Space for your group to focus and develop a network of ideas for your common needs and goals.'
 	},
 	{
 		question: 'How can I make money on Fore?',
-		answer: 'Some say that to succeed you need to be either the smartest or the first. Fore is at the stage in which you can be one of the first. The development of several earning options is on the timeline and the mechanism will be explained further at a later point in time.'
+		answer: 'Some say that to succeed you need to be either the smartest or the first. Fore is at a stage in which you can be one of the first. The development of several earning options is on the timeline and the mechanisms will be explained further at a later point in time.'
 	}
 ];
 
