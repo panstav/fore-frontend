65b12e1f9af057149c59b12438166cd35a016d48
Stav Geffen <stavgeffen@gmail.com>
2023-02-07 12:20:35 +0200

    fix(CurrentFeatures) updates list with new space features

--- src/components/compounds/CurrentFeatures.js
+++ src/components/compounds/CurrentFeatures.js
@@ -10,8 +10,23 @@ export default function CurrentFeatures({ className: classes }) {
 				<li>Browse recent Claims</li>
 				<li>Support or Oppose to existing Claims</li>
 				<li>Power to the best support / opposition of any Claim</li>
-				<li>Copy existing Claims</li>
+				<li>Create new Claims</li>
+				<NewFeature>Create a private Space</NewFeature>
+				<NewFeature>Create a shared Space for up to 12 participants</NewFeature>
 			</ul>
 		</div>
 	</div>;
+}
+
+function NewFeature({ children }) {
+	return <li>
+		<div className="is-flex is-justify-content-space-between is-align-items-center">
+			{children}
+			<New />
+		</div>
+	</li>;
+}
+
+function New() {
+	return <span className="tag is-primary is-size-8 ml-2">NEW</span>;
 }
