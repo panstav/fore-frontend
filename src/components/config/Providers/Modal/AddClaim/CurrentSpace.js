04260d8a569355945c01bed13c5622f71e6b68cf
Stav Geffen <stavgeffen@gmail.com>
2023-02-11 16:53:05 +0200

    fix(AddClaim) sets correct SpaceType icon
    also applies some styles per type

--- src/components/config/Providers/Modal/AddClaim/CurrentSpace.js
+++ src/components/config/Providers/Modal/AddClaim/CurrentSpace.js
@@ -1,18 +1,34 @@
 import { connect } from "unistore/preact";
 
-import { Logo } from 'elements/Icon';
+import { Logo, PrivateSpace, SharedSpace } from 'elements/Icon';
+
+const iconsByType = {
+	private: {
+		Icon: PrivateSpace,
+		transform: 'scale(1.75)'
+	},
+	shared: {
+		Icon: SharedSpace,
+		transform: 'translate(1px, 1px)'
+	},
+	public: {
+		Icon: Logo,
+		transform: ''
+	}
+};
 
 export default connect(mapStateToProps)(CurrentSpace);
 
-function CurrentSpace({ spaceName }) {
-	if (!spaceName) return null;
+function CurrentSpace({ name, type }) {
+	const { Icon, transform } = iconsByType[type];
+	if (!name) return null;
 	return <>
-		<Logo className="ml-1 mr-1" style={{ width: '1em' }} />
-		<span>{spaceName}</span>
+		<Icon className="ml-1 mr-1" style={{ width: '1em', transform }} />
+		<span>{name}</span>
 	</>;
 }
 
 function mapStateToProps({ spaces }) {
-	const { name: spaceName } = spaces.find(({ isCurrent }) => isCurrent);
-	return { spaceName };
+	const { name, type } = spaces.find(({ isCurrent }) => isCurrent);
+	return { name, type };
 }
