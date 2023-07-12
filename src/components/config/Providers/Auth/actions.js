4af293fbb6eeee8a46d1fa94aa90dae99bb41786
Stav Geffen <stavgeffen@gmail.com>
2023-07-12 11:18:06 +0300

    fix(identify) ensure first notifications are immediately sorted by createdAt

--- src/components/config/Providers/Auth/actions.js
+++ src/components/config/Providers/Auth/actions.js
@@ -3,7 +3,9 @@ import api from 'services/xhr';
 export default {
 
 	async identify() {
-		const { user = {}, spaces = [], notifications = [] } = await api.identify();
+		const { user = {}, spaces = [], notifications: unsortedNotifications = [] } = await api.identify();
+		const notifications = unsortedNotifications.sort((a, b) => b.payload.createdAt - a.payload.createdAt);
+
 		return { user, spaces, notifications };
 	}
 
