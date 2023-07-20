352a87c65a05524a25063e06796a0cf13265f0f7
Stav Geffen <stavgeffen@gmail.com>
2023-07-20 18:17:10 +0300

    feat(SpaceInvite) adds a qr once link is available
    (cherry picked from commit c1266d0503f668f417363427b01aa3176a6da6eb)

--- src/lib/css.js
+++ src/lib/css.js
@@ -1,2 +1,4 @@
 export const asideHeaderClasses = 'is-size-6 has-text-weight-bold mb-3';
 export const asideContentClasses = 'is-size-7';
+
+export const primaryColor = getComputedStyle(document.documentElement).getPropertyValue('--color-primary');
