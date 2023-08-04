d244318a4aacf1881007d3d3facd78bf260bc1d1
Stav Geffen <stavgeffen@gmail.com>
2023-08-04 16:47:59 +0300

    wip

--- src/components/config/Providers/Store/index.js
+++ src/components/config/Providers/Store/index.js
@@ -3,7 +3,12 @@ import { Provider, createStore } from 'unistore/full/preact';
 
 import initialState from './initial-state';
 
+import { isDevelopment } from 'constants';
+
 export const store = devtools(createStore(initialState));
+
+if (isDevelopment) ensureImmutability(store);
+
 // store.subscribe(state => {
 // 	// debugger;
 // 	console.log(state);
@@ -13,4 +18,25 @@ export default function StoreProvider({ children }) {
 	return <Provider store={store}>
 		<>{children}</>
 	</Provider>;
+}
+
+function ensureImmutability (store) {
+	store.subscribe(state => deepFreeze(state));
+
+	function deepFreeze(obj) {
+		// Retrieve the property names defined on object
+		const propNames = Reflect.ownKeys(obj);
+
+		// Freeze properties before freezing self
+		for (const name of propNames) {
+			const value = obj[name];
+
+			if ((value && typeof value === "object") || typeof value === "function") {
+				deepFreeze(value);
+			}
+		}
+
+		return Object.freeze(obj);
+	}
+
 }
